3b4c8529dc0232aa151f5d88fbd2ebc8cc0ba818
itaruchan <furukawa3152@gmail.com>
2024-05-11 00:34:46 +0900

    omit_wordを全角でもOKに

--- new_app.py
+++ new_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import re
 from wordcloud import WordCloud
 from janome.tokenizer import Tokenizer
 st.sidebar.text("config")
@@ -10,8 +11,8 @@ theme = st.sidebar.selectbox("theme", ["YlOrRd","gray","YlGnBu", "cool"])
 omit_words = st.sidebar.text_input("除外したいテキスト(スペース区切り)")
 st.title("Word Cloud Maker")
 text = st.text_input("ワードクラウドにしたい文章を入れてね。（500文字以上推奨。）")
-omit_words_list = omit_words.split(" ")
-
+# omit_words_list = omit_words.split(" ")
+omit_words_list = re.split(r'[ 　]+', omit_words) #半角スペース、全角スペースいずれも同じに扱う。
 def nouns_maker(text):
     tokenizer = Tokenizer()
     noun_list = []
